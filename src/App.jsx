d81aca10baa38e60c32d2b2d9675ec5c5cdb16c9
biancabeatrizferenal <chidnoblap444@gmail.com>
2026-09-21 20:48:41 +0800

    1.4: course information step 4

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -26,7 +26,7 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{' '}
-      {props.part1.exercises + props.part2.exercises + props.part3.exercises}
+      {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
     </p>
   )
 }
@@ -44,18 +44,20 @@ const Footer = (props) => {
 const App = () => {
   const course = 'CIT-U Bachelor of Science in Information Technology: Industry Elective'
   
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of component',
+      exercises: 14
+    }
+  ]
 
   const name = 'Bianca Beatriz A. ferenal'
   const courseCode = 'CSIT340'
@@ -64,8 +66,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
       <Footer name={name} courseCode={courseCode} section={section} />
     </div>
   )
